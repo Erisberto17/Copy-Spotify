d7071fda5597b340d66dd3ac3aaf8072a2bdf19b
Eriberto <erislo2001@gmail.com>
2023-01-20 22:56:01 -0300

    another att json

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 async function getData(){
-    const url = "https://my-json-server.typicode.com/Erisberto17/Copy-Spotify/db";
+    const url = "https://my-json-server.typicode.com/Erisberto17/Copy-Spotify/categorias";
 
     const response = await fetch(url);
     const request = await response.json()
-    
-    recieveData(request)
-    
-} 
 
-function recieveData(data){
-    const dataTotal = data.categorias.Rock.albuns.slipknot;
+    recieveData(request);
+
+}; 
+
+function recieveData (data){
     
-    
-    console.log(dataTotal)
-    
-    dataTotal.forEach(element => {
-        console.log(element)
+    data.forEach((e) => {
+        
+        console.log(e)
     });
     
 
