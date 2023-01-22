9d782f08260faca78e076ce519c6361e11a546f9
Eriberto <erislo2001@gmail.com>
2023-01-21 22:19:49 -0300

    another att db.json

--- script.js
+++ script.js
@@ -2,49 +2,52 @@ async function getData(){
     const url = "https://my-json-server.typicode.com/Erisberto17/Copy-Spotify/categorias";
 
     const response = await fetch(url);
-    const request = await response.json()
+    const request = await response.json();
 
     recieveData(request);
 
 }; 
 
 function recieveData (data){
-    const selecao = document.querySelector("#selecao")
+    
+    const selecao = document.querySelector("#selecao");
+    
     data.forEach((e) => {
+
+        const musica = document.querySelector("#musica")
         const titulo = document.createElement("h2");
+
         titulo.innerText = e.titulo;
-        selecao.appendChild(titulo)
+       
         e.albuns.forEach((i) => {
-            /*
+            
             const boxContainer = document.createElement("span");
             const divText = document.createElement("div");
             const img = document.createElement("img");
             const pName = document.createElement("p");
             const pLegend = document.createElement("p");
-
-            img.src = i.img
-            pName.innerText = i.nome;
-            pLegend = i.legend;
-
+                       
             divText.appendChild(pName);
             divText.appendChild(pLegend);
             boxContainer.appendChild(img);
             boxContainer.appendChild(divText);
-
-            selecao.appendChild(boxContainer)
-            */
-
-            console.log(i)
-
-        })
-
-
-       
+            musica.appendChild(boxContainer);
+            
+            img.src = i.img;
+            pName.innerText = i.nome;
+            pLegend.innerText = i.legend;
+            
+            console.log(i);
+            
+        });
+        
+        selecao.appendChild(musica);
+        selecao.appendChild(titulo);
+              
     });
     
-
 };
 
-getData()
+getData();
 
 
